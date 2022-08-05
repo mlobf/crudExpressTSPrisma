5c8dc24c83c82566f52c741c672095bc454b4b17
mlobf <marcoslemeborbafilho@gmail.com>
2022-08-05 00:14:19 -0300

    Add custom error handler middleware

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,7 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import cors from 'cors'
 import routes from './routes/index'
+import AppError from '../errors/appError'
 
 const app = express()
 
@@ -8,6 +9,16 @@ app.use(cors())
 app.use(express.json())
 
 app.use(routes)
+
+app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+  if (error instanceof AppError) response.status(error.statusCode).json({
+    status: 'error',
+    message: error.message
+  })
+
+  return response.status(500).json({ status: 'error', message: 'Internal server error' })
+})
+
 
 app.listen(3333, () => {
   console.log('Server up at port 3333 ')
