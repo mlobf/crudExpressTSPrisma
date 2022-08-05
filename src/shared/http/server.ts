9a40fc5eb6f765d4536c8d93fd2f2db81f6f7b0b
mlobf <marcoslemeborbafilho@gmail.com>
2022-08-04 23:53:04 -0300

    Pre error handler

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,10 +1,15 @@
+import express from 'express'
+import cors from 'cors'
+import routes from './routes/index'
 
-console.log('Hello world!')
+const app = express()
 
+app.use(cors())
+app.use(express.json())
 
+app.use(routes)
 
-
-
-
-
+app.listen(3333, () => {
+  console.log('Server up at port 3333 ')
+})
 
