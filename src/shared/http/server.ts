abe104c7b242e15964a7d18fd2e0719992aadec2
mlobf <marcoslemeborbafilho@gmail.com>
2022-08-12 01:06:42 -0300

    Hotfix  - Now  is running

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -12,14 +12,17 @@ app.use(express.json())
 
 app.use(routes)
 
-app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
-  if (error instanceof AppError) response.status(error.statusCode).json({
-    status: 'error',
-    message: error.message
-  })
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message
+    })
 
-  return response.status(500).json({ status: 'error', message: 'Internal server error' })
-})
+    return response.status(500).json(
+      { status: 'error', message: 'Internal server error' }
+    )
+  })
 
 
 app.listen(3333, () => {
--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -2,20 +2,20 @@ import { createConnection } from 'typeorm'
 
 import '../../ormconfig.json'
 
-const dbConn = {
-  "type": "postgres",
-  "host": "localhost",
-  "port": 5432,
-  "username": "postgres",
-  "password": "secret",
-  "database": "apivendas",
-  "migrations": [
-    "./src/shared/typeorm/migrations/*.ts"
-  ],
-  "cli": {
-    "migrationsDir": "./src/shared/typeorm/migrations/"
+
+createConnection(
+  {
+    "type": "postgres",
+    "host": "localhost",
+    "port": 5432,
+    "username": "postgres",
+    "password": "secret",
+    "database": "apivendas",
+    "migrations": [
+      "./src/shared/typeorm/migrations/*.ts"
+    ],
+    "cli": {
+      "migrationsDir": "./src/shared/typeorm/migrations/"
+    }
   }
-}
-
-
-createConnection(dbConn)
+)
